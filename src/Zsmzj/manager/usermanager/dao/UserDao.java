1d4ed706934366a40468f201f4d66de75bd2c61d
hongmomanu <hongmomanu@gmail.com>
2013-08-13 12:13:34 +0800

    new

--- src/Zsmzj/manager/usermanager/dao/UserDao.java
+++ src/Zsmzj/manager/usermanager/dao/UserDao.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,9 +20,10 @@ import java.util.Map;
  */
 public class UserDao {
     private static final Logger log = Logger.getLogger(UserDao.class);
+    private static String  UserTable="users";
     public ArrayList<Map<String, Object>> getUsers(int start, int limit, String keyword) {
         Connection testConn= JdbcFactory.getConn("sqlite");
-        String sql=  "select username,time,id from users Limit "+limit+" Offset "+ start;
+        String sql=  "select username,time,id from "+UserTable+" Limit "+limit+" Offset "+ start;
         PreparedStatement pstmt = JdbcFactory.getPstmt(testConn, sql);
         ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
         try {
@@ -39,6 +41,28 @@ public class UserDao {
             log.debug(E.getMessage());
             return list;
         }
+
+    }
+
+    public int addnewUser (String username,String password,int roleid){
+
+        Connection conn= JdbcFactory.getConn("sqlite");
+        String sql = "insert  into " + UserTable + " (username,password,roleid) values (?,?,?)  ";
+        PreparedStatement pstmt = JdbcFactory.getPstmt(conn, sql);
+
+        try {
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.setInt(3, roleid);
+
+            //pstmt.setInt(7, Integer.parseInt(params.get("keyid").toString()));
+            return pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            log.debug(ex.getMessage());
+            return -1;
+
+        }
+
 
     }
 
