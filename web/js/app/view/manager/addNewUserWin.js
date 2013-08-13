a588a9b9776965050610132a486cca1ff53e36ad
hongmomanu <hongmomanu@gmail.com>
2013-08-13 10:20:51 +0800

    new

--- web/js/app/view/manager/addNewUserWin.js
+++ web/js/app/view/manager/addNewUserWin.js
@@ -10,12 +10,14 @@ Ext.define('ZSMZJ.view.manager.addNewUserWin' ,{
     alias : 'widget.addnewuserwin',
     requires: [
         //'Ext.form.*'
+        'Ext.Img',
+        'Ext.tip.QuickTipManager'
     ],
     initComponent: function() {
         Ext.apply(this, {
             title: '新增用户',
-            height: 255,
-            width: 300,
+            height: 305,
+            width: 400,
             closeAction : 'hide',
             resizable:false,
             layout: 'fit',
@@ -36,31 +38,166 @@ Ext.define('ZSMZJ.view.manager.addNewUserWin' ,{
                     labelWidth: 100,
                     labelStyle: 'font-weight:bold'
                 },
+        listeners: {
+            fieldvaliditychange: function() {
+                this.updateErrorState();
+            },
+            fielderrorchange: function() {
+                this.updateErrorState();
+            }
+        },
+
+        updateErrorState: function() {
+            var me = this,
+                errorCmp, fields, errors;
+
+            if (me.hasBeenDirty || me.getForm().isDirty()) { //prevents showing global error when form first loads
+                errorCmp = me.down('#formErrorState');
+                fields = me.getForm().getFields();
+                errors = [];
+                fields.each(function(field) {
+                    Ext.Array.forEach(field.getErrors(), function(error) {
+                        errors.push({name: field.getFieldLabel(), error: error});
+                    });
+                });
+                errorCmp.setErrors(errors);
+                me.hasBeenDirty = true;
+            }
+        },
 
                 items: [
                     {
                         xtype: 'textfield',
                         fieldLabel: '用户名',
+                        allowBlank:false,
+                        blankText   : '不能为空',
                         name: 'username'
                     },
                     {
                         xtype: 'textfield',
                         fieldLabel: '用户密码',
+                        blankText   : '不能为空',
+                        allowBlank:false,
+                        inputType: 'password',
                         name: 'password'
                     },
                     {
                         xtype: 'textfield',
                         fieldLabel: '密码确认',
-                        name: 'passwordagain'
+                        inputType: 'password',
+                        allowBlank:false,
+                        name: 'passwordagain',
+                        validator: function(value) {
+                            var password1 = this.previousSibling('[name=password]');
+                            return (value === password1.getValue()) ? true : '前后密码不匹配.'
+                        }
                     }
                     ,
                     {
                         xtype: 'textfield',
                         fieldLabel: '所属角色',
+                        allowBlank:false,
+                        blankText   : '不能为空',
                         name: 'roleid'
                     }
                 ],
-                buttons: [
+
+                dockedItems: [{
+                    cls: Ext.baseCSSPrefix + 'dd-drop-ok',
+                    xtype: 'container',
+                    dock: 'bottom',
+                    layout: {
+                        type: 'hbox',
+                        align: 'middle'
+                    },
+                    padding: '10 10 5',
+
+                    items: [{
+                        xtype: 'component',
+                        id: 'formErrorState',
+                        invalidCls: Ext.baseCSSPrefix + 'form-invalid-icon',
+                        validCls: Ext.baseCSSPrefix + 'dd-drop-icon',
+                        baseCls: 'form-error-state',
+                        flex: 1,
+                        validText: '用户信息合法',
+                        invalidText: '用户信息不合法',
+                        tipTpl: Ext.create('Ext.XTemplate', '<ul class="' + Ext.plainListCls + '"><tpl for="."><li><span class="field-name">{name}</span>: <span class="error">{error}</span></li></tpl></ul>'),
+
+                        getTip: function() {
+                            var tip = this.tip;
+                            if (!tip) {
+                                tip = this.tip = Ext.widget('tooltip', {
+                                    target: this.el,
+                                    title: '错误详情:',
+                                    minWidth: 200,
+                                    autoHide: false,
+                                    anchor: 'top',
+                                    //mouseOffset: [-11, -2],
+                                    closable: true,
+                                    constrain :true,
+                                    constrainPosition: true,
+                                    cls: 'errors-tip'
+                                });
+                                tip.show();
+                            }
+                            return tip;
+                        },
+
+                        setErrors: function(errors) {
+                            var me = this,
+                                tip = me.getTip();
+
+                            errors = Ext.Array.from(errors);
+
+                            // Update CSS class and tooltip content
+                            if (errors.length) {
+                                me.addCls(me.invalidCls);
+                                me.removeCls(me.validCls);
+                                me.update(me.invalidText);
+                                tip.setDisabled(false);
+                                tip.update(me.tipTpl.apply(errors));
+                            } else {
+                                me.addCls(me.validCls);
+                                me.removeCls(me.invalidCls);
+                                me.update(me.validText);
+                                tip.setDisabled(true);
+                                tip.hide();
+                            }
+                        }
+                    }, {
+                        xtype: 'button',
+                        formBind: true,
+                        disabled: true,
+                        text: '提交',
+                        width: 140,
+                        handler: function() {
+                            var form = this.up('form').getForm();
+
+                            /* Normally we would submit the form to the server here and handle the response...
+                             form.submit({
+                             clientValidation: true,
+                             url: 'register.php',
+                             success: function(form, action) {
+                             //...
+                             },
+                             failure: function(form, action) {
+                             //...
+                             }
+                             });
+                             */
+
+                            if (form.isValid()) {
+                                var out = [];
+                                Ext.Object.each(form.getValues(), function(key, value){
+                                    out.push(key + '=' + value);
+                                });
+                                Ext.Msg.alert('Submitted Values', out.join('<br />'));
+                            }
+                        }
+                    }]
+                }],
+
+                /*buttons: [
                     {
                         text: '取消',
                         handler: function () {
@@ -73,7 +210,7 @@ Ext.define('ZSMZJ.view.manager.addNewUserWin' ,{
                         action: 'add'
 
                     }
-                ],
+                ],*/
                 border: false
             }
 
