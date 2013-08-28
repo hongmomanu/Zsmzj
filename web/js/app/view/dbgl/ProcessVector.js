0ad5c6fc6f6a6666f9ba684444a1a141ee1c29d7
hongmomanu <hongmomanu@gmail.com>
2013-08-28 17:31:34 +0800

    jack commit

--- web/js/app/view/dbgl/ProcessVector.js
+++ web/js/app/view/dbgl/ProcessVector.js
@@ -27,7 +27,7 @@ Ext.define('ZSMZJ.view.dbgl.ProcessVector' ,{
                 {
                     type: "text",
                     text: '开始流程',
-                    x:28,
+                    x:25,
                     y:30
 
                 },
@@ -51,7 +51,7 @@ Ext.define('ZSMZJ.view.dbgl.ProcessVector' ,{
                 {
                     type: "text",
                     text: '街道/乡镇审核',
-                    x:170,
+                    x:162,
                     y:30
 
                 },
@@ -62,6 +62,45 @@ Ext.define('ZSMZJ.view.dbgl.ProcessVector' ,{
                     "stroke-width": "1",
                     stroke: "#000",
                     fill: "blue"
+                },
+
+                {
+                    type: "ellipse",
+                    radiusX: 50,
+                    radiusY: 30,
+                    stroke: 'blue',
+                    x: 360,
+                    y: 30
+                },
+                {
+                    type: "text",
+                    text: '区/县/市审核',
+                    x:317,
+                    y:30
+
+                },
+                {
+                    type: "path",
+
+                    path: "M410 28  L460 28 L460 25 L465 30 L460 35 L460 32 L410 32 Z",    //路径      L150 50
+                    "stroke-width": "1",
+                    stroke: "#000",
+                    fill: "blue"
+                },
+                {
+                    type: "ellipse",
+                    radiusX: 50,
+                    radiusY: 30,
+                    stroke: 'blue',
+                    x: 515,
+                    y: 30
+                },
+                {
+                    type: "text",
+                    text: '结束流程',
+                    x:485,
+                    y:30
+
                 }
             ]
 
