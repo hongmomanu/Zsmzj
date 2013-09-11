fd8a36128f6f80ba0e5cfa51c77abd987914c1ee
hongmomanu <hongmomanu@gmail.com>
2013-09-11 17:52:29 +0800

    jack commit

---
+++ web/js/app/model/dbgl/ChangedBusiness.js
@@ -0,0 +1,30 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jack
+ * Date: 13-6-20
+ * Time: 下午5:40
+ * To change this template use File | Settings | File Templates.
+ */
+Ext.define('ZSMZJ.model.dbgl.ChanedBusiness', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'division'},
+        {name:'owername'},
+        {name:'owerid'},
+        {name:'applytype'},
+        {name:'familytype'},
+        {name:'totalhelpmoney',type:'float'},
+        {name:'helpbgtime'},
+        {name:'helpedtime'},
+        {name:'processstatustype'},
+        {name:'familynum',type:'int'},
+        {name:'processstatus'},
+        {name:'process'},
+        {name:'displayname'},
+        {name:'approvaltime'},
+        {name:'approvaluser'},
+        {name:'businessid'}
+
+
+    ]
+});
