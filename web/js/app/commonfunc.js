b9dfa97e2524aa80e24b40b412ff1de2f5b0ee47
weipan789 <weipan789@163.com>
2013-11-11 19:41:18 +0800

    pw comment:

--- web/js/app/commonfunc.js
+++ web/js/app/commonfunc.js
@@ -113,5 +113,40 @@ var CommonFunc = {
     }
 
 
+    ,
+    showBirthday:function (val) {
+        var birthdayValue;
+        if (15 == val.length) { //15位身份证号码
+            birthdayValue = val.charAt(6) + val.charAt(7);
+            if (parseInt(birthdayValue) < 10) {
+                birthdayValue = '20' + birthdayValue;
+            }
+            else {
+                birthdayValue = '19' + birthdayValue;
+            }
+            birthdayValue = birthdayValue + '-' + val.charAt(8) + val.charAt(9) + '-' + val.charAt(10) + val.charAt(11);
+            if (parseInt(val.charAt(14) / 2) * 2 != val.charAt(14))
+                sex = '男';
+            else
+                sex = '女';
+            sr = birthdayValue;
+        }
+        if (18 == val.length) { //18位身份证号码
+            birthdayValue = val.charAt(6) + val.charAt(7) + val.charAt(8) + val.charAt(9) + '-' + val.charAt(10) + val.charAt(11)
+
+                + '-' + val.charAt(12) + val.charAt(13);
+            if (parseInt(val.charAt(16) / 2) * 2 != val.charAt(16))
+                sex = '男';
+            else
+                sex = '女';
+
+            sr = birthdayValue;
+            return {
+                birthday:birthdayValue,
+                sex:sex
+            }
+        }
+
+    }
 
 }
