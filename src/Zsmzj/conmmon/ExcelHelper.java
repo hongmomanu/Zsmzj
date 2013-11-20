f955958d7730b16ec60baf4b13c07689d27fe80b
hongmomanu <hongmomanu@gmail.com>
2013-11-20 10:34:45 +0800

    jack commit

--- src/Zsmzj/conmmon/ExcelHelper.java
+++ src/Zsmzj/conmmon/ExcelHelper.java
@@ -42,6 +42,7 @@ public class ExcelHelper {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        JSONObject sum_item = JSONObject.fromObject(sum);
         if (wwb != null) {
             //创建一个可写入的工作表
             //Workbook的createSheet方法有两个参数，第一个是工作表的名称，第二个是工作表在工作薄中的位置
@@ -60,8 +61,19 @@ public class ExcelHelper {
                 urlparamsstr+="limit=100000&start=0";
 
                 rowdatas=JSONObject.fromObject(UrlConnectHelper.sendPost(url,urlparamsstr)).getJSONArray("results");
+                Map<String,Double> sum_new=new HashMap<String, Double>();
+                for (Object sum_name : sum_item.names()) {
+                    sum_new.put(sum_name.toString(),0.0);
+
+                }
 
                 for (int row_index = 0; row_index < rowdatas.size(); row_index++) {
+
+                    for (Object sum_name : sum_item.names()) {
+                        sum_new.put(sum_name.toString(),sum_new.get(sum_name.toString())+
+                                Double.parseDouble(JSONObject.fromObject(rowdatas.get(row_index)).getString(sum_name.toString())));
+
+                    }
                     String division=JSONObject.fromObject(rowdatas.get(row_index)).getString("division");
                     String sql="select parentid,divisionname from divisions where divisionpath MATCH '"+division+"'";
                     ComonDao cd=new ComonDao();
@@ -90,12 +102,13 @@ public class ExcelHelper {
                     rowdatas.set(row_index, row);
 
                 }
+                sum_item=JSONObject.fromObject(sum_new);
 
 
             }else{
                 rowdatas = JSONArray.fromObject(rowdata);
             }
-            JSONObject sum_item = JSONObject.fromObject(sum);
+
             try {
                 WritableFont font = new WritableFont(WritableFont.createFont("宋体"),
                         15,
@@ -218,9 +231,7 @@ public class ExcelHelper {
 
                         if (col_name.equals(sum_name.toString())) {
                             labelSumC = new Label(j, sumrow_index, sum_item.get(sum_name).toString());
-                            Formula f1 = new Formula(j, sumrow_index, "SUM(C29:D29)");
                             labelSumC.setCellFormat(cellRowFormat);
-                            log.debug(labelSumC.getContents());
                             ws.addCell(labelSumC);
                             break;
                         }
