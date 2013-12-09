c799bc6b772df450e439141d77fb38a875db1289
weipan789 <weipan789@163.com>
2013-12-09 09:15:17 +0800

    pw

---
+++ web/js/app/view/propertycheck/familymemberFieldset.js
@@ -0,0 +1,49 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jack
+ * Date: 13-10-23
+ * Time: 下午2:39
+ * To change this template use File | Settings | File Templates.
+ */
+Ext.define('ZSMZJ.view.propertycheck.familymemberFieldset', {
+    extend : 'Ext.form.FieldSet',
+    alias : 'widget.propertycheckfamilymemberfieldset',
+    requires: [
+
+
+    ],
+
+    initComponent: function() {
+        var required = '<span style="color:red;font-weight:bold" data-qtip="必填字段">*</span>';
+        Ext.apply(this,
+            {
+                xtype: 'fieldset',
+                title: '<a>家庭成员信息</a>',
+                cls:'fieldset-border',
+                defaultType: 'textfield',
+                bodyStyle: 'padding:5px 5px 5px 5px',
+                layout: 'fit',
+
+                items:[
+                    /* {
+                     xtype:'panel',
+                     layout: 'fit',
+                     items:[*/
+                    {
+                        xtype:'propertycheckfamilymembergrid',
+                        //itemId:'propertycheckfamilymembergrid'
+                        itemId:'familymembergrid'
+
+                    }
+                    /* ]
+
+                     }*/
+
+                ]
+            }
+        );
+        this.callParent(arguments);
+
+    }
+
+});
