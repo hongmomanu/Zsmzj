a8a36fcb4272a91a4d82175b5f48359b21130207
hongmomanu <hongmomanu@gmail.com>
2014-01-26 13:53:37 +0800

    jack commit

---
+++ web/jsnew/commonfuncs/ButtonsEvents/appformsubmit_signature.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jack on 14-1-6.
+ */
+
+define(function(){
+
+    var a={
+        render:function(item,datares){
+
+            console.log(item);
+            console.log(datares);
+
+        }
+
+    }
+
+    return a;
+});
