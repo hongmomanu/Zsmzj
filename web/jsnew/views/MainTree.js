77018dda93049a74b5479e976bc565552eb443d1
hongmomanu <hongmomanu@gmail.com>
2014-02-09 09:14:18 +0800

    jack commit

--- web/jsnew/views/MainTree.js
+++ web/jsnew/views/MainTree.js
@@ -6,22 +6,20 @@ define(function(){
     var a={
 
         render:function(parameters){
-            $('#dbglacc,#dbedgeacc').tree({
+            $('#menu_shjz .navigation-tree').tree({
                 //url:'tree_data.json'
                 onClick: function (node){
 
                     var tree=$(this);
-                    var businesstype=tree.attr('businesstype')
+                    var businesstype=tree.attr('businesstype');
                     var me=this;
                     if(tree.tree('isLeaf', node.target)){
-                            //var selectid=$('#tabs').tabs('select',1).panel('options').id;
                         var isnew=false;
                         if($('#tabs').tabs('exists',1)){
                             $('#tabs').tabs('select',1);
                             isnew=$('#tabs').tabs('getSelected').panel('options').id!=node.id;
                         }
                         if(!$('#tabs').tabs('exists',1)||me.nodeid!=node.id||isnew){
-
                             var folder=tree.attr('folder');
                             var htmlfile='text!'+folder+node.value+'.htm';
                             var jsfile=folder+node.value;
