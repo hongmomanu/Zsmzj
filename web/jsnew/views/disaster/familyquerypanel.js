347239be10d96184041d8ddb2f4ebc0f2b381f97
hongmomanu <hongmomanu@gmail.com>
2014-02-11 12:36:41 +0800

    jack commit

--- web/jsnew/views/disaster/familyquerypanel.js
+++ web/jsnew/views/disaster/familyquerypanel.js
@@ -5,12 +5,7 @@ define(function () {
 
     function render(parameters) {
 
-       /* var columns=[familyheaders.dbgl];
-        var businesstype=$('#tabs').tabs('getSelected').panel('options').businesstype;
-        var type=businesstype;
-        require(['commonfuncs/BusinessGridCommon','jqueryplugin/jquery-formatDateTime'],function(BusinessGridCommon){
-            BusinessGridCommon.initbusinessgrid(type,businesstype,columns);
-        });*/
+
         require(['views/dbgl/dbglgrantmoneypanel'],function(js){
             js.render();
         });
