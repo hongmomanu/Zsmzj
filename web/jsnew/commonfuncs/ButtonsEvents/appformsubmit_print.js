6c593caa3a01e6b4f9dec6f5168b14f495383b03
hongmomanu <hongmomanu@gmail.com>
2014-02-13 14:05:08 +0800

    jack commit

--- web/jsnew/commonfuncs/ButtonsEvents/appformsubmit_print.js
+++ web/jsnew/commonfuncs/ButtonsEvents/appformsubmit_print.js
@@ -6,7 +6,7 @@ define(function(){
 
     var a={
         render:function(item,datares){
-            $.messager.alert('提示','此接口为完成!');
+            $.messager.alert('提示','我的位置在‘commonfuncs/ButtonsEvents/appformsubmit_print.js’此接口留给魏潘了，参见源程序‘ZSMZJ.view.dbgl.businessPrint’模块!');
         }
 
     }
